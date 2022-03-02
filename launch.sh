116efd720cae7645c181de7748ff00bb9a422046
carlosgual <carlos.gutierrezalva@uah.es>
2022-03-02 12:44:23 +0100

    edited launch.sh to launch the ROS processes

--- launch.sh
+++ launch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sh ~/lola_navigation_ws/devel/setup.bash
+./home/rober/lola_navigation_ws/devel/setup.bash
 roslaunch lola2_global basic_lola.launch
 roslaunch logitech_f710_joy_ros joy_teleop.launch
 cd ~/gui4lola/ || exit
