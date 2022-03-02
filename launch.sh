139c4e0432ed8d90811a46342d4ea1b336751c0b
carlosgual <carlos.gutierrezalva@uah.es>
2022-03-02 12:52:05 +0100

    added multiple programs to run simultaneously

--- launch.sh
+++ launch.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-./home/rober/lola_navigation_ws/devel/setup.bash
-roslaunch lola2_global basic_lola.launch
-roslaunch logitech_f710_joy_ros joy_teleop.launch
+roslaunch lola2_global basic_lola.launch &
+roslaunch logitech_f710_joy_ros joy_teleop.launch &
 cd ~/gui4lola/ || exit
 python3 main.py
