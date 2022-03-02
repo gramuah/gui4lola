dcd08bb9bc2c6b9c14bdb3dc49ae9c2b6625d3ef
carlosgual <carlos.gutierrezalva@uah.es>
2022-03-02 14:00:52 +0100

    trying with sh only

--- launch.sh
+++ launch.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-{ roslaunch lola2_global basic_lola.launch && \
-roslaunch logitech_f710_joy_ros joy_teleop.launch && \
-python3 ~/gui4lola/main.py ;}
+{ roslaunch lola2_global basic_lola.launch & \
+roslaunch logitech_f710_joy_ros joy_teleop.launch & \
+python3 ~/gui4lola/main.py &;}
