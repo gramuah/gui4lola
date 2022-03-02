c609fbbaa908f419f2367a62a1afd3d697be44c1
carlosgual <carlos.gutierrezalva@uah.es>
2022-03-02 13:20:45 +0100

    deleted roslaunch from sh

--- launch.sh
+++ launch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-gnome-terminal -e roslaunch lola2_global basic_lola.launch &
-gnome-terminal -e roslaunch logitech_f710_joy_ros joy_teleop.launch &
+# gnome-terminal -e roslaunch lola2_global basic_lola.launch &
+# gnome-terminal -e roslaunch logitech_f710_joy_ros joy_teleop.launch &
 cd ~/gui4lola/ || exit
 python3 main.py
